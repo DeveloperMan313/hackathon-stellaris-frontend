aad076e9a3624b2502640715fe91c9be192f773b
DeveloperMan313 <053000voloshch@gmail.com>
2025-10-26 15:36:27 +0300

    Add /comets mock

---
+++ src/lib/templates/CometCard.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Card from "$lib/components/ui/card/card.svelte";
+</script>
+
+<Card class="w-full pt-0 pb-2 gap-2">
+  <img class="size-48 bg-purple-500" src="" alt="comet name" />
+  <h4 class="px-4 text-center line-clamp-2 h-[2lh]">Comet name</h4>
+</Card>
---
+++ src/routes/comets/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Button from "$lib/components/ui/button/button.svelte";
+  import Input from "$lib/components/ui/input/input.svelte";
+  import CometCard from "$lib/templates/CometCard.svelte";
+  import Navbar from "$lib/templates/Navbar.svelte";
+</script>
+
+<Navbar />
+<div class="w-full mt-40 px-5 flex flex-col items-center gap-10">
+  <div class="flex flex-row gap-2 h-10">
+    <Input
+      type="text"
+      placeholder="Название кометы"
+      class="w-96 h-full shrink-0"
+    />
+    <Button class="h-full cursor-pointer">Найти</Button>
+  </div>
+  <div class="w-full grid grid-cols-6 gap-6">
+    {#each [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] as i (i)}
+      <CometCard />
+    {/each}
+  </div>
+</div>
